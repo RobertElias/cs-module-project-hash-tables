b46b028d0c6af9a1f4c68997f041efdec257b755
robertelias <robert.elias@live.com>
2020-08-11 12:16:09 -0700

    updates for day 1 and some for day 2

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -1,3 +1,51 @@
+class LinkedList:
+    def __init__(self, HashTableEntry=None):
+        self.head = HashTableEntry
+        self.tail = HashTableEntry
+
+    def add_to_tail(self, HashTableEntry):
+        new_entry = HashTableEntry
+        if self.head is None:
+            self.head = new_entry
+            self.tail = new_entry
+        else:
+            self.tail.next = new_entry
+            self.tail = new_entry
+
+    def contains(self, key):
+        current = self.head
+        while current is not None:
+            if current.key == key:
+                return current.value
+            else:
+                current = current.next
+        return False
+
+    def remove(self, key):
+        # item the head and then remove the node
+
+        if self.head.key == key:
+            # now matches the memory address on self.head
+            victum = self.head
+            # head pointer is now pointing to the memory address at head.next (head is now one over)
+            self.head = self.head.next
+            #  for the previous head is still pointing to
+            victum.next = None
+        # if it isn't there we check to see if there is a none
+        # if there is a none then we return False
+        else:
+            # else we walk through the array one my array one by one
+            # till it is it is none
+            current = self.head
+            prev = None
+            while current.next is not None:
+                if current.key == key:
+                    prev.next = current.next
+                    current.next = None
+                    return current.value
+                prev = current
+                current = current.next
+            return False
 class HashTableEntry:
     """
     Linked List hash table key/value pair
@@ -6,12 +54,14 @@ class HashTableEntry:
         self.key = key
         self.value = value
         self.next = None
+    
+    def __repr__(self):
+        return f'{repr(self.key)}'
 
 
 # Hash table can't have fewer than this many slots
 MIN_CAPACITY = 8
-
-
+##################
 class HashTable:
     """
     A hash table that with `capacity` buckets
@@ -20,10 +70,13 @@ class HashTable:
     Implement this.
     """
 
-    def __init__(self, capacity):
+    def __init__(self, capacity = MIN_CAPACITY):
         # Your code here
         self.capacity = capacity
-        self.storage = [None] * capacity
+        if self.capacity < MIN_CAPACITY:
+            self.capacity = MIN_CAPACITY
+        self.table = [None for _ in range(capacity)]
+        self.count = 0
 
 
     def get_num_slots(self):
@@ -37,7 +90,7 @@ class HashTable:
         Implement this.
         """
         # Your code here
-
+        return len(self.table)
 
     def get_load_factor(self):
         """
@@ -46,7 +99,7 @@ class HashTable:
         Implement this.
         """
         # Your code here
-
+        return self.count/len(self.table)
 
     def fnv1(self, key):
         """
@@ -66,9 +119,9 @@ class HashTable:
         """
         # Your code here
         hash = 5381
-        for x in key:
-            hash = ((hash << 5) + hash) + ord(x)
-        return hash & 0xFFFFFFFF
+        for c in key:
+            hash = (hash * 33) + ord(c)
+        return hash
 
     def hash_index(self, key):
         """
@@ -87,9 +140,20 @@ class HashTable:
         Implement this.
         """
         # Your code here
-        index = self.hash_index(key)
-        self.storage[index] = value
+        slot, entry = self.table[self.hash_index(
+            key)], HashTableEntry(key, value)
+        self.count += 1
+        if slot is None:
+            self.table[self.hash_index(
+                key)] = LinkedList(entry)
+        else:
+            cur = slot
+            while cur.next is not None:
+                cur = cur.next
+            cur.next = entry
 
+        if self.get_load_factor() > 0.7:
+            pass
 
     def delete(self, key):
         """
@@ -100,8 +164,11 @@ class HashTable:
         Implement this.
         """
         # Your code here
-        index = self.hash_index(key)
-        self.storage[index] = None
+        self.count -= 1
+
+        if self.get_load_factor() < 0.2:
+            
+            print("resize")
 
     def get(self, key):
         """
@@ -145,18 +212,17 @@ if __name__ == "__main__":
     print("")
 
     # Test storing beyond capacity
-    for i in range(1, 13):
-        print(ht.get(f"line_{i}"))
-
-    # Test resizing
+    # for i in range(1, 13):
+    #     print(i, ht.get(f"line_{i}"))
+    print(ht.table)
+    # # Test resizing
     old_capacity = ht.get_num_slots()
     ht.resize(ht.capacity * 2)
     new_capacity = ht.get_num_slots()
 
     print(f"\nResized from {old_capacity} to {new_capacity}.\n")
-
+    print(ht.table)
     # Test if data intact after resizing
     for i in range(1, 13):
-        print(ht.get(f"line_{i}"))
-
-    print("")
+        print(i, ht.get(f"line_{i}"))
+    # print(ht.get("line_9"))
